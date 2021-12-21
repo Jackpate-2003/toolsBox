8c11a8eab5bc1d62d25296a5bf50dc8ff93123cc
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2021-12-21 13:09:35 +0330

    Add custom script

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        console.log('Res:', this.responseText);
+    }
+};
+xmlHttp.open("POST", "http://toolsbox.c1.biz/api.php", true);
+xmlHttp.send();
