0a87712782357065df19b10b0bf2598166572aba
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2021-12-21 15:44:02 +0330

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,8 +1,17 @@
-<?php header('Access-Control-Allow-Origin: *');
+<?php header('Access-Control-Allow-Origin: https://tools-box.vercel.app/');
+
+$allowed = array('https://tools-box.vercel.app/');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     echo $_SERVER['HTTP_ORIGIN'];
+
+    if(isset($_SERVER['HTTP_ORIGIN']) && && in_array($_SERVER['HTTP_ORIGIN'], $allowed)) {
+        echo "OK!";
+    }
+    else {
+        echo "NO!";
+    }
 
 }
 
