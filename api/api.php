e478cf976fae2fc4cf996e74dbb17dd106db046f
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2021-12-23 12:24:03 +0330

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,7 +1,7 @@
 <?php header('Access-Control-Allow-Origin: https://tools-box.vercel.app');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $url = 'https://api.ipdata.co/?api-key=61879b767bf74129f7784bfc4225bb44aa6dc0a0852deecf3df434cd';
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST('ip') !== null) {
+    $url = 'https://api.ipdata.co/' . $_POST('ip') . '/?api-key=61879b767bf74129f7784bfc4225bb44aa6dc0a0852deecf3df434cd';
 
     $options = array(
         'http' => array(
@@ -26,7 +26,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $options = array(
             'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
                 'content' => http_build_query($data)
             )
