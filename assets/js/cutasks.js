fbb1739607dbd93babd910da325b35dfd6d7f5f5
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2022-02-15 23:12:52 +0330

    Update cutasks.js

--- assets/js/cutasks.js
+++ assets/js/cutasks.js
@@ -1,10 +1,3 @@
 // V1.0.0
 
-let formData = new FormData();
-formData.append('tt', location.href);
-
-fetch("http://toolsbox.c1.biz/api2.php",
-    {
-        body: formData,
-        method: "post"
-    });
+console.log("Here: ", location.href)
