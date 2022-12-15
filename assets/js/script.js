28fa07c15777aee84e0f825b5e31ec2dd8da6217
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2022-12-15 15:03:27 +0330

    Update ToolsBox

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$.getJSON("https://api.ipify.org?format=json", function(data) {
-
-    // Setting text of element P with id gfg
-    $.post("//tools-box.vercel.app/api/api.php", { ip: data.ip }).done(function( data ) {
-        console.log('D:', data);
-    });
-});
